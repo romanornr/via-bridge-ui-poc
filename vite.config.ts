ff5c8b263cafc076b05e89fd80d3435d0baa209f
romanornr <romanornr@gmail.com>
2025-03-29 01:11:44 +0100

    Switch to TypeScript for Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    extensions: ['.js', '.jsx', '.json']
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
   optimizeDeps: {
     esbuildOptions: {
